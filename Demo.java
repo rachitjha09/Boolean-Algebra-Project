d697c2163a5967f1df8b3147d66ec35bf8eb8f63
rachitjha09 <rachitjha09@gmail.com>
2024-09-02 18:53:22 -0400

    Add files via upload
    Boolean Algebra Practice

---
+++ Demo.java
@@ -0,0 +1,17 @@
+
+/**
+ * Boolean Algebra Project
+ *
+ * @author (Rachit Jha)
+ * @version (10/27/23)
+ *  //a&&a = a
+    //b+b = b
+    //a&&!a = 0
+    //b+!b = 1
+    //a || (a&&b) = a
+    //a&&(b||c) = a && b+a && c
+ */
+public class Demo
+{
+
+}
